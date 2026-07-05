660a8b07fe6c5f67c831e03d1ffe7f45f0c3ab44
Hasan Khan <hasan109.khan@gmail.com>
2026-07-05 13:35:45 +0000

    variables

---
+++ 01_Basics/variables.js
@@ -0,0 +1,11 @@
+//constants -> cannot be modified
+const accId = 123456
+
+//prefered method is to use let to declare variables
+let accEmail = "hasan109.khan@gmail.com"
+
+//avoid using var due to scope problems
+var accPass = "5458"
+accCity = "karachi"
+
+console.table([accId, accEmail, accPass, accCity])
